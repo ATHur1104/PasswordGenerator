4020dfdd36bb57eb7b8e78e66d35918f16461d41
Andrew <ATHur1104@gmail.com>
2023-04-18 22:25:51 -0500

    Added basic password generator

--- assets/script.js
+++ assets/script.js
@@ -3,21 +3,27 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  function generatePassword() {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var length = prompt("Please choose number of characters."); 
-    var password = ""; 
-for (let i = 0; i < length; i++) {
-  var rand = Math.floor(Math.random() * chars.length); 
-  password += chars();
+  var length = prompt("Please enter the number of characters you would like."); 
+  if (length < 8 || length > 128) {
+    alert("Please choose a number between 8 and 128");
+    return;
   }
+  var password = generatePassword(length);
+  var passwordText = document.querySelector("#password");
+
+  function generatePassword(length) {
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var password = "";
+    for (let i = 0; i < length; i++) {
+      var rand = Math.floor(Math.random() * chars.length);
+      password += chars[rand];
+    }
   return password;
 }
-  function generate() {passwordText.value = password;
+  passwordText.value = password;
 }
-}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
 
@@ -36,8 +42,7 @@ for (let i = 0; i < length; i++) {
 
 
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+
 // // Assignment Code
 // var generateBtn = document.querySelector("#generate");
 
