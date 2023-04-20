8d91862d56653cfada4f0870fce7024d59ecc158
Andrew <ATHur1104@gmail.com>
2023-04-20 02:24:14 -0500

    cleaning up safety net

--- script.js
+++ script.js
@@ -87,36 +87,3 @@ function writePassword() {
     }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
